d6744f47161d2fa9b36ea766f54b8a78522a672b
Usuario <Usuario@10.22.148.84>
2019-09-25 09:43:55 +0100

    comentarios

--- src/kata1/Person.java
+++ src/kata1/Person.java
@@ -18,7 +18,8 @@ public class Person {
     private final static int MS_PER_MINUT=60;
     private final static int MS_PER_HOUR=60;
     private final static double MS_PER_DAY=24;
-    private final static double MS_PER_YEAR=365.25;
+    private final static double Days_PER_YEAR=365.25;
+    private final static double MS_PER_YEAR= (int) (MS_PER_DAY*MS_PER_HOUR*MS_PER_MINUT*MS_PER_SECOND*Days_PER_YEAR);
     public Person(String name, Date birthDay) {
         this.name = name;
         this.birthDay = birthDay;
